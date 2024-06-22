c7f2c0443fd5f49c2ca2bb2e06a5fe80af83b8e8
Chuckshallcode <charleycooling@gmail.com>
2024-06-22 07:47:47 -0500

    Changes to be committed: 	new file:   .classpath 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs     new file:   .settings/org.eclipse.m2e.core.prefs 	new file:   pom.xml 	new file:   projectsGit.png 	new file:   src/main/java/projects/Projects.java 	new file:   src/main/java/projects/dao/DbConnection.java 	new file:   src/main/java/projects/exception/DbException.java 	new file:   src/main/resources/projects-schema.sql 	new file:   target/classes/META-INF/MANIFEST.MF 	new file:   target/classes/META-INF/maven/com.promineo/projects.java/pom.properties 	new file:   target/classes/META-INF/maven/com.promineo/projects.java/pom.xml 	new file:   target/classes/projects-schema.sql 	new file:   target/classes/projects/Projects.class 	new file:   target/classes/projects/dao/DbConnection.class 	new file:   target/classes/projects/exception/DbException.class  commit

---
+++ src/main/java/projects/Projects.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package projects;
+
+import projects.dao.DbConnection;
+
+/**
+ * 
+ */
+public class Projects {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DbConnection.getConnection();
+	}
+
+}
